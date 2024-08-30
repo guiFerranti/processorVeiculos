31a39ec464f477aaafc842d3d60c2889c8978077
guiFerranti <guilhermecousseau@icloud.com>
2024-08-30 00:18:17 -0300

    feat: adicionando configuracao do autoMapper

---
+++ src/Backend/Processor.Veiculos.Application/Services/AutoMapper/AutoMapping.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using Processor.Veiculos.Communication.Requests;
+
+namespace Processor.Veiculos.Application.Services.AutoMapper;
+
+public class AutoMapping : Profile
+{
+    public AutoMapping()
+    {
+        RequestToDomain();
+    }
+
+    private void RequestToDomain()
+    {
+        CreateMap<RequestRegisteredVeiculoJson, Domain.Entities.Veiculo>();
+    }
+
+    private void DomainToResponse() {}
+}
