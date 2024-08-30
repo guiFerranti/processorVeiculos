7db3b61ddc14a58e05e9e6da652ce69dfc302833
guiFerranti <guilhermecousseau@icloud.com>
2024-08-30 00:22:45 -0300

    feat: adicionando classe para salvar veiculos em JSON

---
+++ src/Backend/Processor.Veiculos.Domain/Repositories/Veiculos/IVeiculoWriteOnlyRepository.cs
@@ -0,0 +1,8 @@
+﻿using Processor.Veiculos.Domain.Entities;
+
+namespace Processor.Veiculos.Domain.Repositories.Veiculos;
+
+public interface IVeiculoWriteOnlyRepository
+{
+    public Task Add(Entities.Veiculo veiculo);
+}
