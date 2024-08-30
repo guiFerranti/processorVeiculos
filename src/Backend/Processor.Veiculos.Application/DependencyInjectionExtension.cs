4e436e5f00f1e6af30382de68310a44f19cb17cf
guiFerranti <guilhermecousseau@icloud.com>
2024-08-30 00:21:38 -0300

    feat: adicionando injetor de dependencias da Application e da Infrasctructure

---
+++ src/Backend/Processor.Veiculos.Application/DependencyInjectionExtension.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper;
+using Microsoft.Extensions.DependencyInjection;
+using Processor.Veiculos.Application.Services.AutoMapper;
+using Processor.Veiculos.Application.UseCases.Veiculos.Register;
+using Processor.Veiculos.Domain.Repositories.Veiculos;
+
+namespace Processor.Veiculos.Application;
+
+public static class DependencyInjectionExtension
+{
+    public static void AddAplication(this IServiceCollection services)
+    {
+        AddAutoMapper(services);
+        AddUseCases(services);
+    }
+
+    private static void AddAutoMapper(IServiceCollection services)
+    {
+        var autoMapper = new MapperConfiguration(cfg =>
+        {
+            cfg.AddProfile(new AutoMapping());
+        }).CreateMapper();
+
+        services.AddScoped(cfg => autoMapper);
+    }
+
+    private static void AddUseCases(IServiceCollection services)
+    {
+        services.AddScoped<IRegisterVeiculoUseCase, RegisterVeiculoUseCase>();
+    }
+}
---
+++ src/Backend/Processor.Veiculos.Infrastructure/DependencyInjectionExtension.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Processor.Veiculos.Domain.Repositories.Veiculos;
+using Processor.Veiculos.Infrastructure.DataAccess.Repositories;
+using System.Runtime.CompilerServices;
+
+namespace Processor.Veiculos.Infrastructure;
+
+public static class DependencyInjectionExtension
+{
+    public static void AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    {
+        AddRepositories(services, configuration);
+    }
+
+    private static void AddRepositories(IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddScoped<IVeiculoWriteOnlyRepository, VeiculoRepository>(provider =>
+        {
+            var filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Data", "veiculos.json");
+            return new VeiculoRepository(filePath);
+        });
+    }
+}
