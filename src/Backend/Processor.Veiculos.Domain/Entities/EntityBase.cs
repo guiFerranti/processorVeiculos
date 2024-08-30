4a1ae4be3f4736f18a22b1a17d60002fcbdfc763
guiFerranti <guilhermecousseau@icloud.com>
2024-08-29 22:28:00 -0300

    feat: adicionando Entidade Base e Veiculo

---
+++ src/Backend/Processor.Veiculos.Domain/Entities/EntityBase.cs
@@ -0,0 +1,8 @@
+﻿namespace Processor.Veiculos.Domain.Entities;
+
+public class EntityBase
+{
+    public long Id { get; set; }
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+}
---
+++ src/Backend/Processor.Veiculos.Domain/Entities/Veiculo.cs
@@ -0,0 +1,8 @@
+﻿namespace Processor.Veiculos.Domain.Entities;
+
+public class Veiculo : EntityBase
+{
+    public int Ano { get; set; }
+    public string Modelo { get; set; } = string.Empty;
+    public string Marca { get; set; } = string.Empty;
+}
