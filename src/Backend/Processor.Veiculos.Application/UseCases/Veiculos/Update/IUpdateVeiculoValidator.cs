2eeca53339f7893e85f7032995b1c1a449ecd6da
guiFerranti <guilhermecousseau@icloud.com>
2024-08-30 12:56:01 -0300

    feat: adicionando validator para o UpdateVeiculo

---
+++ src/Backend/Processor.Veiculos.Application/UseCases/Veiculos/Update/IUpdateVeiculoValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using Processor.Veiculos.Communication.Requests;
+using Processor.Veiculos.Exceptions;
+
+namespace Processor.Veiculos.Application.UseCases.Veiculos.Update;
+
+public class UpdateVeiculoValidator : AbstractValidator<RequestUpdateVeiculoJson>
+{
+    public UpdateVeiculoValidator()
+    {
+        int anoAtual = DateTime.Now.Year;
+        int anoLimite = anoAtual - 20;
+
+        RuleFor(veiculo => veiculo.Marca).NotEmpty().WithMessage(ResourceMessagesException.MARCA_EMPTY);
+        RuleFor(veiculo => veiculo.Modelo).NotEmpty().WithMessage(ResourceMessagesException.MODELO_EMPTY);
+        RuleFor(veiculo => veiculo.Ano).NotEmpty().WithMessage(ResourceMessagesException.ANO_EMPTY);
+        RuleFor(veiculo => veiculo.Ano).GreaterThanOrEqualTo(anoLimite).WithMessage(ResourceMessagesException.ANO_LIMITE_INVALID);  
+    }
+}
--- src/Backend/Processor.Veiculos.Application/UseCases/Veiculos/Update/UpdateVeiculo.cs
+++ src/Backend/Processor.Veiculos.Application/UseCases/Veiculos/Update/UpdateVeiculo.cs
@@ -1,6 +1,8 @@
 ﻿using AutoMapper;
+using Processor.Veiculos.Application.UseCases.Veiculos.Register;
 using Processor.Veiculos.Communication.Requests;
 using Processor.Veiculos.Domain.Repositories.Veiculos;
+using Processor.Veiculos.Exceptions.ExceptionsBase;
 
 namespace Processor.Veiculos.Application.UseCases.Veiculos.Update;
 
@@ -17,8 +19,24 @@ public class UpdateVeiculo : IUpdateVeiculo
 
     public Task Execute(long id, RequestUpdateVeiculoJson veiculo)
     {
+        Validate(veiculo);
+
         var veiculoAtualizado = _mapper.Map<Domain.Entities.Veiculo>(veiculo);
 
         return _veiculoRepository.Update(id, veiculoAtualizado);
+    }
+
+    private void Validate(RequestUpdateVeiculoJson request)
+    {
+        var validator = new UpdateVeiculoValidator();
+
+        var result = validator.Validate(request);
+
+        if (!result.IsValid)
+        {
+            var errorMessages = result.Errors.Select(error => error.ErrorMessage).ToList();
+
+            throw new ErrorsOnValidationException(errorMessages);
+        }
     }
 }
