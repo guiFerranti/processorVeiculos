68cc8bf001cc6dbb49fd20b2a3ec66a46b6818d8
guiFerranti <guilhermecousseau@icloud.com>
2024-08-30 00:24:52 -0300

    feat: adicionando implementacao da funcao de salvar Veiculos

---
+++ src/Backend/Processor.Veiculos.Infrastructure/DataAccess/Repositories/VeiculoRepository.cs
@@ -0,0 +1,38 @@
+﻿using Processor.Veiculos.Domain.Entities;
+using Processor.Veiculos.Domain.Repositories.Veiculos;
+using System.Text.Json;
+
+namespace Processor.Veiculos.Infrastructure.DataAccess.Repositories;
+
+public class VeiculoRepository : IVeiculoWriteOnlyRepository
+{
+    private readonly string _filePath;
+    public VeiculoRepository(string filePath)
+    {
+        _filePath = filePath;
+    }
+
+
+    public async Task Add(Veiculo veiculo)
+    {
+        List<Veiculo> veiculos;
+
+        if (File.Exists(_filePath))
+        {
+            var jsonString = await File.ReadAllTextAsync(_filePath);
+
+            veiculos = JsonSerializer.Deserialize<List<Veiculo>>(jsonString) ?? new List<Veiculo>();
+        }
+        else
+        {
+            veiculos = new List<Veiculo>();
+        }
+
+        veiculos.Add(veiculo);
+
+        var newJsonString = JsonSerializer.Serialize(veiculos, new JsonSerializerOptions { WriteIndented = true });
+
+        await File.WriteAllTextAsync(_filePath, newJsonString);
+
+    }
+}
