f8224950165081b8e01e0cd37a1643ff805ae624
guiFerranti <guilhermecousseau@icloud.com>
2024-08-30 10:24:47 -0300

    feat: adicionando prioridade de atributos na escrita do Json

--- src/Backend/Processor.Veiculos.Domain/Entities/EntityBase.cs
+++ src/Backend/Processor.Veiculos.Domain/Entities/EntityBase.cs
@@ -1,8 +1,15 @@
-﻿namespace Processor.Veiculos.Domain.Entities;
+﻿using System.Text.Json.Serialization;
+
+namespace Processor.Veiculos.Domain.Entities;
 
 public class EntityBase
 {
+    [JsonPropertyOrder(1)]
     public long Id { get; set; }
+
+    [JsonPropertyOrder(2)]
     public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+    [JsonPropertyOrder(3)]
     public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
 }
--- src/Backend/Processor.Veiculos.Domain/Entities/Veiculo.cs
+++ src/Backend/Processor.Veiculos.Domain/Entities/Veiculo.cs
@@ -1,8 +1,15 @@
-﻿namespace Processor.Veiculos.Domain.Entities;
+﻿using System.Text.Json.Serialization;
+
+namespace Processor.Veiculos.Domain.Entities;
 
 public class Veiculo : EntityBase
 {
+    [JsonPropertyOrder(4)]
     public int Ano { get; set; }
+
+    [JsonPropertyOrder(5)]
     public string Modelo { get; set; } = string.Empty;
+
+    [JsonPropertyOrder(6)]
     public string Marca { get; set; } = string.Empty;
 }
