d70643f04b978f70319344c5d555f2c731ba9f23
guiFerranti <guilhermecousseau@icloud.com>
2024-08-30 22:49:08 -0300

    feat: adicionando componentes de paginacao ao DTO do getAll Veiculos

--- src/Shared/Processor.Veiculos.Communication/Responses/ResponseGetAllVeiculosJson.cs
+++ src/Shared/Processor.Veiculos.Communication/Responses/ResponseGetAllVeiculosJson.cs
@@ -3,4 +3,8 @@
 public class ResponseGetAllVeiculosJson
 {
     public IEnumerable<ResponseGetVeiculoJson> Veiculos { get; set; } = new List<ResponseGetVeiculoJson>();
+
+    public int TotalCount { get; set; }
+    public int PageNumber { get; set; }
+    public int PageSize { get; set; }
 }
