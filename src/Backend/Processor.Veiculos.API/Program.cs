ec6b3e3882ccbeed582eabc7f57743d8da315576
guiFerranti <guilhermecousseau@icloud.com>
2024-09-09 13:28:40 -0300

    feat: adicionando leitura da enviroment variable PORT ao Program

--- src/Backend/Processor.Veiculos.API/Program.cs
+++ src/Backend/Processor.Veiculos.API/Program.cs
@@ -6,6 +6,9 @@ using System.Text.Json;
 
 var builder = WebApplication.CreateBuilder(args);
 
+var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
+builder.WebHost.UseUrls($"http://*:{port}");
+
 // Add services to the container.
 
 builder.Services.AddControllers();
